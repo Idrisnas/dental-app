22f88f4eb4042c8b9fc57156d754b6b31f179df5
Idris Mohammed <nasiru.idris4646@gmail.com>
2024-09-15 18:37:01 +0100

    aded

--- js/script.js
+++ js/script.js
@@ -168,3 +168,11 @@ function toggleMenu() {
     menu.classList.toggle('active');
 }
 
+        function myFunction() {
+          var x = document.getElementById("myLinks");
+          if (x.style.display === "block") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "block";
+          }
+        }
